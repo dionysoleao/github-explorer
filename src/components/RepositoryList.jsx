5bd815712644664f152b540be8302319f919e578
dionyso <dionysoleao@gmail.com>
2021-12-15 18:00:00 -0500

    complemento do 1o dia de código no github-explorer

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { RepositoryItem } from "./RepositoryItem";
+
+const repositoryInfo = {
+    name: 'Unform',
+    desc: 'Forms in React',
+    link: 'Acessar Repositório'
+}
+//
+export function RepositoryList(){
+
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, [])
+
+    return(
+        <section className="repository-list">
+        <h1>Lista de Repositórios</h1>
+        <ul>
+            {repositories.map(repository => {
+                return <RepositoryItem key={repository.name} repository={repository} />
+                }
+              )
+            }
+        </ul>
+        </section>
+    );
+}
